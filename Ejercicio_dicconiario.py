0ce3112f5986c01aaf4bab1c37e24bf65edee33f
Kisame4800 <itcloud.crojas@gmail.com>
2024-08-31 15:18:11 -0500

    Update Ejercicio_dicconiario.py
    cambie algo...

--- Ejercicio_dicconiario.py
+++ Ejercicio_dicconiario.py
@@ -3,8 +3,8 @@ meme_dict = {
             "LOL": "Una respuesta común a algo gracioso",
             "FUNAR": "Denuncia publica de muchas personas hacia alguien",
             "WTF": "Abreviación the What The Fuck",
-            "Roto/a": "Algo o alguien en un juego cuyas habilidades son muy altas y superiores",
-            "Nerfear": "Bajar de nivel de poder o habilidad algo o alguien que tenía poder muy alto"
+            "ROTO/A": "Algo o alguien en un juego cuyas habilidades son muy altas y superiores",
+            "NERFEAR": "Bajar de nivel de poder o habilidad algo o alguien que tenía poder muy alto"
             }
             
 word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
